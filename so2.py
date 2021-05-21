ec2b25e30ab490a5cad64c3439993096730bf51b
Agstarte <aga1999.1@gmail.com>
2021-05-21 20:17:31 +0200

    Cykl dnia, ulepszona klasa Person

--- so2.py
+++ so2.py
@@ -1,10 +1,14 @@
 import random
 import logging
 import time
+import threading
 
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO,
                     datefmt="%H:%M:%S")
+
+daytime = threading.Event()
+people_sent = threading.Semaphore(0)
 
 
 # logging.getLogger().setLevel(logging.DEBUG)
@@ -13,14 +17,25 @@ logging.basicConfig(format=format, level=logging.INFO,
 
 class AbstractSector(object):
     # todo jakiekolwiek wspólne funkcje tutaj
+    _list_of_people = list()
 
     def __init__(self, inhabitants=0, materials=0, food=0):
         logging.info("Creating an object of %s.", type(self).__name__)
+        if inhabitants != 0 and type(self) is not LivingSector:
+            logging.error("Sector other than LivingSector cannot have inhabitants initially.")
         self._inhabitants = inhabitants
         self._materials = materials
         self._food = food
         if type(self) is AbstractSector:
             raise NotImplementedError("Can't create an object of an abstract class.")
+        self.startup()
+
+    def startup(self):
+        pass
+
+    def start(self):
+        thread = threading.Thread(target=self.process)
+        thread.start()
 
     def get_resources(self):
         print(
@@ -47,27 +62,75 @@ class AbstractSector(object):
     def get_type(self):
         return type(self).__name__
 
+    def take_people(self, new_people: list):
+        self._list_of_people += new_people
+
+    def send_people(self):
+        temp = self._list_of_people
+        self._list_of_people = list()
+        return temp
+
+    def process(self):
+        pass
+
 
 class LivingSector(AbstractSector):
-    # todo ogarnianie ludzi - proces
-    def a(self):
-        pass
+    # todo zarządzanie ludźmi - proces
+    def startup(self):
+        for n in range(self._inhabitants):
+            self._list_of_people.append(Person(n))
+
+    def process(self):
+        while True:
+            daytime.wait()
+            logging.info("living_sector\t\t : \tSend the people off.")
+            people_sent.release()
+            people_sent.release()
+
+            while daytime.isSet():
+                pass
+            people_sent.acquire()
+            people_sent.acquire()
+            logging.info("living_sector\t\t : \tFeed people.")
 
 
 class MaterialSector(AbstractSector):
     # todo wytwarzanie materiałów - proces
-    def a(self):
-        pass
+    def process(self):
+        while True:
+            daytime.wait()
+
+            people_sent.acquire()
+            logging.info("material_sector\t : \tGet people.")
+            while daytime.isSet():
+                logging.info("material_sector\t : \tProducing materials.")
+                time.sleep(2)
+            logging.info("material_sector\t : \tSend off people.")
+            time.sleep(0.1)
+            people_sent.release()
 
 
 class FoodSector(AbstractSector):
     # todo wytwarzanie jedzenia - proces
-    def a(self):
-        pass
+    def process(self):
+        while True:
+            daytime.wait()
+
+            people_sent.acquire()
+            logging.info("food_sector\t\t : \tGet people.")
+            while daytime.isSet():
+                logging.info("food_sector\t\t : \tProducing food.")
+                time.sleep(2)
+            logging.info("food_sector\t\t : \tSend off people.")
+            time.sleep(0.1)
+            people_sent.release()
 
 
 class Person(object):
     # todo starzenie się, jedzenie + to ile ma siły w zależności ile zjadł
+    _age = 0
+    _is_alive = True
+
     def __init__(self, name=0):
         logging.info("Creating an object of %s.", type(self).__name__)
         self._days = random.randrange(100, 150)
@@ -83,12 +146,28 @@ class Person(object):
     def get_name(self):
         return self._name
 
+    def get_age(self):
+        return self._age
+
+    def is_alive(self):
+        return self._is_alive
+
     def print_person(self):
         print(
             f'Person \t\t:\t {self._name}\n'
+            f'Is alive \t:\t {self._is_alive}\n'
             f'Work speed \t:\t {self._work_speed}\n'
+            f'Age \t\t:\t {self._age}\n'
             f'Days \t\t:\t {self._days}\n'
         )
+
+    def age_up(self):
+        self._age += 1
+        if self._age >= self._days:
+            self.die()
+
+    def die(self):
+        self._is_alive = False
 
 
 class Cart(object):
@@ -137,6 +216,30 @@ class Cart(object):
             return True
 
 
+def day_cycle():
+    while True:
+        logging.info("day_cycle\t\t\t : \tDay is raising!")
+        daytime.set()
+        time.sleep(16)
+        logging.info("day_cycle\t\t\t : \tNight has come...")
+        daytime.clear()
+        time.sleep(8)
+
+
+def start_sectors():
+    logging.info("start_sectors\t\t : \tStarting processes.")
+    living_sector = LivingSector(inhabitants=100)
+    material_sector = MaterialSector(materials=199)
+    food_sector = FoodSector(food=1299)
+
+    living_sector.start()
+    material_sector.start()
+    food_sector.start()
+
+    day = threading.Thread(target=day_cycle)
+    day.start()
+
+
 # living_sector = LivingSector()
 # material_sector = MaterialSector()
 #
@@ -160,38 +263,47 @@ class Cart(object):
 #     cart.move_cart(material_sector, living_sector)
 
 
-
 # abstract = Abstract()
-a = LivingSector(inhabitants=100)
-time.sleep(0.1)
-a.get_resources()
+# a = LivingSector(inhabitants=100)
+# time.sleep(0.1)
+# a.get_resources()
+#
+# time.sleep(0.1)
+#
+# a.update_inhabitants(-29)
+# time.sleep(0.1)
+# a.get_resources()
+#
+# time.sleep(0.1)
+#
+# a.update_materials(2334)
+# time.sleep(0.1)
+# a.get_resources()
+#
+# time.sleep(0.1)
+#
+# a.update_food(2909)
+# time.sleep(0.1)
+# a.get_resources()
+#
+# time.sleep(0.1)
+#
+# a = MaterialSector(materials=199)
+# time.sleep(0.1)
+# a.get_resources()
+#
+# time.sleep(0.1)
+#
+# a = FoodSector(food=1299)
+# time.sleep(0.1)
+# a.get_resources()
 
-time.sleep(0.1)
+# start_sectors()
 
-a.update_inhabitants(-29)
-time.sleep(0.1)
-a.get_resources()
+a = LivingSector(inhabitants=10)
 
-time.sleep(0.1)
-
-a.update_materials(2334)
-time.sleep(0.1)
-a.get_resources()
-
-time.sleep(0.1)
-
-a.update_food(2909)
-time.sleep(0.1)
-a.get_resources()
-
-time.sleep(0.1)
-
-a = MaterialSector(materials=199)
-time.sleep(0.1)
-a.get_resources()
-
-time.sleep(0.1)
-
-a = FoodSector(food=1299)
-time.sleep(0.1)
-a.get_resources()
+people = a.send_people()
+for person in people:
+    for i in range(person.get_days()):
+        person.age_up()
+    person.print_person()
